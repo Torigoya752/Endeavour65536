762d578d1c20c36162dd89a07cd6ddcd720d9057
jack.chen <xun_02@163.com>
2025-10-20 18:08:13 +0800

    simplify tryFrom* logic

--- board.py
+++ board.py
@@ -13,6 +13,45 @@ logging.basicConfig(
 class Err65536(Exception):
     pass
 
+# load qLegal
+with open ("./qLegal/up.txt", "r") as f:
+    lines = f.readlines()
+QCANFROMUP = []
+for line in lines:
+    QCANFROMUP.append(int(line.rstrip()))
+
+
+# down
+with open ("./qLegal/down.txt", "r") as f:
+    lines = f.readlines()
+QCANFROMDOWN = []
+for line in lines:
+    QCANFROMDOWN.append(int(line.rstrip()))
+
+
+# left
+with open ("./qLegal/left.txt", "r") as f:
+    lines = f.readlines()
+QCANFROMLEFT = []
+for line in lines:
+    QCANFROMLEFT.append(int(line.rstrip()))
+
+
+# right
+with open ("./qLegal/right.txt", "r") as f:
+    lines = f.readlines()
+QCANFROMRIGHT = []
+for line in lines:
+    QCANFROMRIGHT.append(int(line.rstrip()))
+    
+# exist
+with open ("./qLegal/exist.txt", "r") as f:
+    lines = f.readlines()
+QEXIST = []
+for line in lines:
+    QEXIST.append(int(line.rstrip()))
+
+
 class Q:
     def __init__(self, list_1, rate):
         self.board = copy.deepcopy(list_1)
@@ -21,66 +60,63 @@ class Q:
         self.canFromDown = True
         self.canFromLeft = True
         self.canFromRight = True
+        self.containNumberMask = 0
+        for i in range(4):
+            for j in range(4):
+                if(self.board[i][j] != 0):
+                    self.containNumberMask += (1 << (i*4 + j))
         
     def tryFromUp(self):
-        for i in range(3):
-            for j in range(4):
-                if(self.board[i][j] == 0 and self.board[i+1][j] != 0):
-                    self.canFromUp = False
-                    break
+        if(self.containNumberMask not in QCANFROMUP):
+            self.canFromUp = False
+            return
                 
         for j in range(4):
             if(self.board[2][j]!= 0 and self.board[3][j] == 0 and self.board[0][j] == self.board[1][j] and self.board[1][j] == self.board[2][j]):
                 self.canFromUp = False
-                break
+                return
             if(self.board[3][j] != 0 and (self.board[0][j] == self.board[1][j] or self.board[1][j] == self.board[2][j] or self.board[2][j] == self.board[3][j])):
                 self.canFromUp = False
-                break
+                return
             
     def tryFromDown(self):
-        for i in range(1,4):
-            for j in range(4):
-                if(self.board[i][j] == 0 and self.board[i-1][j] != 0):
-                    self.canFromDown = False
-                    break
+        if(self.containNumberMask not in QCANFROMDOWN):
+            self.canFromDown = False
+            return
                 
         for j in range(4):
             if(self.board[1][j]!= 0 and self.board[0][j] == 0 and self.board[2][j] == self.board[3][j] and self.board[1][j] == self.board[2][j]):
                 self.canFromDown = False
-                break
+                return
             if(self.board[0][j] != 0 and (self.board[1][j] == self.board[2][j] or self.board[2][j] == self.board[3][j] or self.board[1][j] == self.board[0][j])):
                 self.canFromDown = False
-                break
+                return
                 
     def tryFromLeft(self):
-        for i in range(4):
-            for j in range(3):
-                if(self.board[i][j] == 0 and self.board[i][j+1] != 0):
-                    self.canFromLeft = False
-                    break
+        if(self.containNumberMask not in QCANFROMLEFT):
+            self.canFromLeft = False
+            return
                 
         for i in range(4):
             if(self.board[i][2] != 0 and self.board[i][0] == 0 and self.board[i][1] == self.board[i][2]):
                 self.canFromLeft = False
-                break
+                return
             if(self.board[i][3] != 0 and (self.board[i][1] == self.board[i][2] or self.board[i][2] == self.board[i][3] or self.board[i][1] == self.board[i][0])):
                 self.canFromLeft = False
-                break
+                return
                 
     def tryFromRight(self):
-        for i in range(4):
-            for j in range(1,4):
-                if(self.board[i][j] == 0 and self.board[i][j-1] != 0):
-                    self.canFromRight = False
-                    break
+        if(self.containNumberMask not in QCANFROMRIGHT):
+            self.canFromRight = False
+            return
                 
         for i in range(4):
             if(self.board[i][1] != 0 and self.board[i][3] == 0 and self.board[i][2] == self.board[i][1]):
                 self.canFromRight = False
-                break
+                return
             if(self.board[i][0] != 0 and (self.board[i][2] == self.board[i][1] or self.board[i][1] == self.board[i][0] or self.board[i][2] == self.board[i][3])):
                 self.canFromRight = False
-                break
+                return
         
         
 
